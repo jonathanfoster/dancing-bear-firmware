01469b7dbc8d1b5bdd8eb06a0bbdb2c04e185bae
Jonathan Foster <jonathan@jonathanfoster.io>
2022-09-01 14:37:00 -0400

    Only call button press handler on press and release

--- src/controls/button.cpp
+++ src/controls/button.cpp
@@ -2,20 +2,16 @@
 
 #include <Arduino.h>
 
-#include "logging/logger.h"
-
 namespace controls {
-
-logging::Logger* logger = new logging::Logger("button");
 
 Button::Button(uint8_t pin) {
   this->_pin = pin;
   this->setup();
 }
 
-Button::Button(uint8_t pin, int default_state) {
+Button::Button(uint8_t pin, int default_pin_state) {
   this->_pin = pin;
-  this->_default_state = default_state;
+  this->_default_pin_state = default_pin_state;
   this->setup();
 }
 
@@ -24,31 +20,33 @@ unsigned long Button::debounce() { return this->_debounce; }
 void Button::debounce(unsigned long value) { this->_debounce = value; }
 
 void Button::checkState() {
-  int current_state = digitalRead(this->_pin);
-  if (current_state != this->_last_state) {
+  bool current_is_pressed = this->checkIsPressed();
+  if (current_is_pressed != this->_last_is_pressed) {
+    this->_last_is_pressed = current_is_pressed;
     this->_last_debounce = millis();
   }
 
   if (millis() - this->_last_debounce > this->_debounce) {
-    if (current_state != this->_state) {
-      this->_state = current_state;
+    if (current_is_pressed == this->_is_pressed) {
+      return;
     }
 
-    if (this->_state != this->_default_state) {
+    if (this->_is_pressed && !current_is_pressed) {
       if (this->_press_handler) {
         this->_press_handler();
       }
     }
-  }
 
-  this->_last_state = current_state;
+    this->_is_pressed = current_is_pressed;
+  }
 }
 
 void Button::onPress(void (*handler)()) { this->_press_handler = handler; }
 
-void Button::setup() {
-  pinMode(this->_pin, INPUT);
-  this->_last_state = this->_default_state;
+bool Button::checkIsPressed() {
+  return digitalRead(this->_pin) != this->_default_pin_state;
 }
+
+void Button::setup() { pinMode(this->_pin, INPUT); }
 
 }  // namespace controls
--- src/controls/button.h
+++ src/controls/button.h
@@ -8,21 +8,22 @@ namespace controls {
 class Button {
  public:
   Button(uint8_t pin);
-  Button(uint8_t pin, int default_state);
+  Button(uint8_t pin, int default_pin_state);
   void checkState();
   unsigned long debounce();
   void debounce(unsigned long value);
   void onPress(void (*handler)());
 
  private:
+  bool checkIsPressed();
   void setup();
 
   unsigned long _debounce = 50;
   unsigned long _last_debounce = 0;
-  int _state = LOW;
-  int _last_state = LOW;
+  bool _is_pressed = false;
+  bool _last_is_pressed = false;
   uint8_t _pin;
-  int _default_state = HIGH;
+  int _default_pin_state = HIGH;
   void (*_press_handler)();
 };
 
